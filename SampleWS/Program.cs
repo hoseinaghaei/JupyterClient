8f9c5bca0bd6e388a0a2002295d2e816821f5c33
mostmand <mostmand.mh@gmail.com>
2021-07-26 17:59:33 +0430

    Some refactoring

--- SampleWS/Program.cs
+++ SampleWS/Program.cs
@@ -18,81 +18,59 @@ namespace SampleWS
         private static string CSharpKernel => ".net-csharp";
         private static string PythonKernel => "python3";
 
+        private const string XsrfHeaderKey = "X-XSRFToken";
+        private const string XsrfCookieKey = "_xsrf";
+        private const string XsrfToken = "2%7C700b3e6c%7C00863d280e479c2310a79ca3c0cf7fe3%7C1626703811";
+
+        private const string LoginCookieKey = "username-localhost-8888";
+        private const string LoginCookieValue = "2|1:0|10:1627303366|23:username-localhost-8888|44:YWM5OGYwNGVlMGIyNGQ0MDk3OGYzOTRjZGMwNmM3NjI=|734984294192067c95b042198543c7eb907d8200bde8a2f8c47de1d2a348befe";
+        private const string Password = "m.jupyter";
+
 
         private static readonly string CommId = Guid.NewGuid().ToString();
 
         static async Task Main(string[] args)
         {
-            // var kernelId = "10ff846c-f93e-4207-880e-2b9fe3faf455";
             var kernelId = await StartKernel(CSharpKernel);
-
-            // var sessionId = "544c7e73-b5c6-45fe-bd3b-ec184c6614fa";
             var sessionId = await StartSession(kernelId, CSharpKernel);
+            
+            var webSocket = await ConnectToKernelAsync(kernelId);
+            
+            Console.WriteLine($"WebSocket state: {webSocket.State}");
+            
+            var message = CreateJupyterMessage(sessionId);
+            var json = JsonConvert.SerializeObject(message);
+            await webSocket.SendAsync(Encoding.UTF8.GetBytes(json), WebSocketMessageType.Text, true, default);
+            await Receive(webSocket);
+        }
 
-            // await StartSession(kernelId);
-            // var http = new HttpClient();
-            // var httpRequestMessage = new HttpRequestMessage(HttpMethod.Post, "http://localhost:8888/api/kernels")
-            // {
-            //     Content = new StringContent(JsonConvert.SerializeObject(new StartKernel() {name = "net-csharp"})),
-            //     Headers = {{"X-XSRFToken", "2|5965414d|3bb094199a7fc8d24c9f910bc623e14e|1626098682"}},
-            // };
-            // var responseMessage = await http.SendAsync(httpRequestMessage);
-            //
-            // var content = await responseMessage.Content.ReadAsStringAsync();
-            // var res = JsonConvert.DeserializeObject<StartKernelResponse>(content);
-
-            var cts = new CancellationTokenSource();
-            // const string kernelId = "788e2414-6a9a-420b-bb30-fd09a1eebd19";
-            //const string token = "e18ea91ce003cf8d29785bccf801ccc783d096b0f17262a3";
-            // var uri = $"ws://localhost:8888/api/kernels/{kernelId}/channels?token={token}";
-            // var uri = $"ws://localhost:8888/api/kernels/{kernelId}/channels";
+        private static async Task<ClientWebSocket> ConnectToKernelAsync(string kernelId)
+        {
             var uri = $"ws://localhost:8888/api/kernels/{kernelId}/channels";
-
             var uri2 = new Uri(uri);
 
             var webSocket = new ClientWebSocket();
             webSocket.Options.Cookies = new CookieContainer();
-            webSocket.Options.Cookies.Add(new Cookie("username-localhost-8888",
-                "2|1:0|10:1627120645|23:username-localhost-8888|44:OTRkMzE1YzU1M2RhNDQzOTllNjNmNDA2NWU2MGE2YTY=|1745520ca85f96381babd44b5d2674f02365afcc457ad98efc62421b2902adf9",
-                "/",
-                uri2.Host)
-            {
-                Expires = new DateTime(2025, 10, 2)
-            });
-            webSocket.Options.Cookies.Add(new Cookie("_xsrf", "2|5965414d|3bb094199a7fc8d24c9f910bc623e14e|1626098682", "/", uri2.Host)
-            {
-                Expires = new DateTime(2025, 10, 2)
-            });
-
-            //Console.WriteLine(webSocket.State);
-            await webSocket.ConnectAsync(uri2, cts.Token);
-            Console.WriteLine(webSocket.State);
-            var message = CreateJupyterMessage(sessionId);
-            //var message = CreateJupyterMessage2();
-            var json = JsonConvert.SerializeObject(message);
-            await webSocket.SendAsync(Encoding.UTF8.GetBytes(json), WebSocketMessageType.Text, true, cts.Token);
-            // message = CreateJupyterMessage3();
-            // json = JsonConvert.SerializeObject(message);
-            // Console.WriteLine(json);
-            // await webSocket.SendAsync(Encoding.UTF8.GetBytes(json), WebSocketMessageType.Text, true, cts.Token);
-            await Receive(webSocket);
+            webSocket.Options.Cookies.Add(GetLoginCookie());
+            webSocket.Options.Cookies.Add(GetXsrfCookie());
+            await webSocket.ConnectAsync(uri2, default);
+            return webSocket;
         }
 
         private static async Task<string> StartSession(string kernelId, string kernelName)
         {
             var baseAddress = new Uri("http://localhost:8888/api/sessions");
             var cookieContainer = new CookieContainer();
-            cookieContainer.Add(baseAddress, new Cookie("username-localhost-8888",
-                "2|1:0|10:1627120645|23:username-localhost-8888|44:OTRkMzE1YzU1M2RhNDQzOTllNjNmNDA2NWU2MGE2YTY=|1745520ca85f96381babd44b5d2674f02365afcc457ad98efc62421b2902adf9",
-                "/",
-                "localhost"));
-            cookieContainer.Add(new Cookie("_xsrf", "2|5965414d|3bb094199a7fc8d24c9f910bc623e14e|1626098682", "/", "localhost"));
+            foreach (var cookie in GetCookies())
+            {
+                cookieContainer.Add(cookie);
+            }
             using var handler = new HttpClientHandler() {CookieContainer = cookieContainer};
             using var client = new HttpClient(handler) {BaseAddress = baseAddress};
             var httpRequestMessage1 = new HttpRequestMessage(HttpMethod.Post, baseAddress)
             {
                 Content = new StringContent(GetSessionContent(kernelId, kernelName)),
-                Headers = {{"X-XSRFToken", "2|5965414d|3bb094199a7fc8d24c9f910bc623e14e|1626098682"}},
+                Headers = {{XsrfHeaderKey, XsrfToken}},
             };
             var responseMessage = await client.SendAsync(httpRequestMessage1);
             var content = await responseMessage.Content.ReadAsStringAsync();
@@ -104,39 +82,66 @@ namespace SampleWS
         {
             return JsonConvert.SerializeObject(new Session()
             {
-                id = kernelId,
-                name = "",
-                path = "",
+                // id = kernelId,
+                // name = "",
+                path = "asghar.ipynb",
                 type = "notebook",
                 kernel = new Kernel()
                 {
                     id = kernelId,
-                    name = kernelName,
-                    connections = 1
+                    // name = kernelName,
+                    // connections = 1
                 }
             });
+        }
+
+        private static IEnumerable<Cookie> GetCookies()
+        {
+            return new List<Cookie>()
+            {
+                GetLoginCookie(),
+                GetXsrfCookie()
+            };
         }
 
         private static async Task<string> StartKernel(string kernelName)
         {
             var baseAddress = new Uri("http://localhost:8888/api/kernels");
             var cookieContainer = new CookieContainer();
-            cookieContainer.Add(baseAddress, new Cookie("username-localhost-8888",
-                "2|1:0|10:1627120645|23:username-localhost-8888|44:OTRkMzE1YzU1M2RhNDQzOTllNjNmNDA2NWU2MGE2YTY=|1745520ca85f96381babd44b5d2674f02365afcc457ad98efc62421b2902adf9",
-                "/",
-                "localhost"));
-            cookieContainer.Add(new Cookie("_xsrf", "2|5965414d|3bb094199a7fc8d24c9f910bc623e14e|1626098682", "/", "localhost"));
+            foreach (var cookie in GetCookies())
+            {
+                cookieContainer.Add(cookie);
+            }
             using var handler = new HttpClientHandler() {CookieContainer = cookieContainer};
             using var client = new HttpClient(handler) {BaseAddress = baseAddress};
             var httpRequestMessage1 = new HttpRequestMessage(HttpMethod.Post, baseAddress)
             {
                 Content = new StringContent(JsonConvert.SerializeObject(new StartKernel() {name = kernelName})),
-                Headers = {{"X-XSRFToken", "2|5965414d|3bb094199a7fc8d24c9f910bc623e14e|1626098682"}},
+                Headers = {{XsrfHeaderKey, XsrfToken}}
             };
             var responseMessage = await client.SendAsync(httpRequestMessage1);
             var content = await responseMessage.Content.ReadAsStringAsync();
             var res = JsonConvert.DeserializeObject<StartKernelResponse>(content);
             return res.id;
+        }
+
+        private static Cookie GetXsrfCookie()
+        {
+            return new Cookie(XsrfCookieKey, XsrfToken, "/", "localhost")
+            {
+                Expires = DateTime.Now.AddYears(1)
+            };
+        }
+
+        private static Cookie GetLoginCookie()
+        {
+            return new Cookie(LoginCookieKey,
+                LoginCookieValue,
+                "/",
+                "localhost")
+            {
+                Expires = DateTime.Now.AddYears(1)
+            };
         }
 
         private static async Task Receive(WebSocket socket)
@@ -173,10 +178,10 @@ namespace SampleWS
                 header = new JupyterMessage.Header()
                 {
                     date = DateTime.Now,
-                    msg_id = "a7b59eab-705c98c3f8d941fc2f6c26d7_55",
+                    msg_id = "asghar",
                     msg_type = JupyterMessage.Header.MsgType.execute_request,
                     session = sessionId,
-                    username = "hosseinaghaei",
+                    // username = "hosseinaghaei",
                     //username = "username",
                     version = "5.3"
                     //version = "5.2"
@@ -187,7 +192,7 @@ namespace SampleWS
                     allow_stdin = true,
                     //code = "a = 5\nprint(\"The Result : \", a**2)",
                     //code = "count = 0\nfor i in range(int(1e10)):\n  count+=1\nprint(count)",
-                    code = "display(\"bos back\");",
+                    code = "display(\"bos back\")",
                     silent = false,
                     stop_on_error = true,
                     store_history = true,
