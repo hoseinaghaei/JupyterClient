20f585c8e6460ab8b667c3028c7e80db46c0bc20
amirhessam <amirhessamb123@yahoo.com>
2021-08-16 15:15:47 +0430

    Exceptions Added

---
+++ SampleWS/JupyterFileHandler/JupyterFileHandler.cs
@@ -0,0 +1,9 @@
+namespace SampleWS.JupyterFileHandler
+{
+    //todo myHttpClient
+    public class JupyterFileHandler : IJupyterFileHandler
+    {
+        private JupyterFileManager _fileManager;
+        
+    }
+}
